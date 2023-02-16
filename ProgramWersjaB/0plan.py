52397f2856ec76affd65e57c31d909526192835c
bojarskifbox <116755294+bojarskifbox@users.noreply.github.com>
2023-02-16 09:41:52 +0100

    Delete ProgramWersjaB directory

--- ProgramWersjaB/0plan.py
+++
@@ -1,6 +0,0 @@
-"""
-1. automatyczne uruchomienie programu przez harmonogram zadań windows
-2. logowanie, pobranie pliku, sortowanie pliku, sprawdzenie
-3. odczyt danych z pliku xlsx, zapis danych w odpowiedniej formie do tabeli pośredniej access,
-4. zatwierdzanie wpisów automatyczne i reczne do bazy, sprawdzanie okresowe
-"""
